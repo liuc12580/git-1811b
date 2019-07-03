1496635ec7cf35cb3ec77abc539611e72153edd7
liuc12580 <958616386@qq.com>
2019-07-03 14:39:18 +0800

    添加了一个假日实体类

---
+++ src/main/java/com/jk/model/Zhongqiu.java
@@ -0,0 +1,26 @@
+package com.jk.model;
+
+import java.util.Date;
+
+public class Zhongqiu {
+
+    private String name;
+
+    private Date dateTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+}
